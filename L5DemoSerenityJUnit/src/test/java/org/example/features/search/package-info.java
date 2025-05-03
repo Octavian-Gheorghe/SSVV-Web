7437e023a77c843b0e6421de25c39dc4effaec96
unknown <gheorgheoctavian2022@gmail.com>
2025-05-03 14:24:00 +0300

    removed old cs.ubbcluj.ro tests and implemented new tests for the lab5 requirment

--- L5DemoSerenityJUnit/src/test/java/org/example/features/search/package-info.java
+++ L5DemoSerenityJUnit/src/test/java/org/example/features/search/package-info.java
@@ -1,9 +1,7 @@
 
 @Narrative(
-        title = "Search for definitions",
-        text = {"In order to impress people",
-                "As a party goer",
-                "I want to be able to look up the meaning of unusual and impressive words"},
+        title = "Search for products based on categories",
+        text = {},
         cardNumber = "#123"
 )
 package org.example.features.search;
